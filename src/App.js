6361ee7876a14acb3d6a662bccfb347b64075ddd
ebarrsim <72933481+ebarrsim@users.noreply.github.com>
2021-02-01 14:54:45 +0000

    Created foundation for Weather
    Foundation levels including fake data, form and icon ready for api to be put in.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import Weather from "./Weather"
 
 function App() {
   return (
     <div className="App">
       <div className="container">
         <h1>Weather App</h1>
-      <button className="btn btn-primary">Hello</button>
+      <Weather />
       <footer>
         This website was created by Eleanor Barr-Sim and is {" "} 
       <a href="https://github.com/ebarrsim/ebarrsim-weather-react" target="_blank">
