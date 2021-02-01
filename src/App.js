7013216abb50730fbcd6b2ba77ed42bb9a9e4aac
ebarrsim <72933481+ebarrsim@users.noreply.github.com>
2021-02-01 14:14:45 +0000

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -6,6 +6,12 @@ function App() {
       <header className="App-header">
       Weather App
       </header>
+      <footer>
+        This website was created by Eleanor Barr-Sim and is {" "} 
+      <a href="https://github.com/ebarrsim/ebarrsim-weather-react" target="_blank">
+      open-sourced on Github
+      </a>
+      </footer>
     </div>
   );
 }
